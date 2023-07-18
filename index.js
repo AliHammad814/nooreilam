28c339736f3cda074ea2736aa2bac7693b8dccd3
AliHammad814 <97762682+AliHammad814@users.noreply.github.com>
2023-07-18 16:22:03 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,51 @@
+// const mongoose = require('mongoose')
+// // mongoose.connect('mongodb://127.0.0.1:27017')
+// mongoose.connect('mongodb+srv://AliHammad:aliali786@cluster0.t8p9lpu.mongodb.net')
+
+// const express = require('express')
+// const app = express()
+// app.use(express.static('/public'))
+
+
+
+
+
+
+// const user_routes = require('./routes/user')
+// const admin_routes = require('./routes/admin')
+
+// app.use('/' , user_routes)
+// app.use('/admin' , admin_routes)
+
+
+
+// app.listen(3000 , ()=>{
+//     console.log('server is running')
+// })
+
+
+
+
+
+
+require('dotenv').config()
 const mongoose = require('mongoose')
-// mongoose.connect('mongodb://127.0.0.1:27017')
-mongoose.connect('mongodb+srv://AliHammad:aliali786@cluster0.t8p9lpu.mongodb.net')
+const PORT = process.env.PORT || 3000
+
+mongoose.set('strictQuery' , false)
+const connectDB = async () => {
+    try {
+      const conn = await mongoose.connect(process.env.MONGO_URI);
+      console.log(`MongoDB Connected: ${conn.connection.host}`);
+    } catch (error) {
+      console.log(error);
+      process.exit(1);
+    }
+  }
+
+// mongoose.connect('mongodb+srv://AliHammad:aliali786@cluster0.t8p9lpu.mongodb.net')
+
+
 
 const express = require('express')
 const app = express()
@@ -19,6 +64,13 @@ app.use('/admin' , admin_routes)
 
 
 
-app.listen(3000 , ()=>{
-    console.log('server is running')
+// app.listen(5000 , ()=>{
+//     console.log('server is running')
+// })
+
+connectDB().then(() => {
+    app.listen(PORT, () => {
+        console.log("listening for requests");
+    })
 })
+
